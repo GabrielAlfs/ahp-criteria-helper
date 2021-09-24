cd2d911ea0e0afb3215bdd7b710a8d8e9ca15ac6
Gabriel Alves <l.luis.gabriel@outlook.com>
2021-09-24 12:27:38 -0300

    feat: add export of methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('Hello world');
+import { calculateConsistency } from './calculate-consistency';
+import { calculateWeights } from './calculate-weights';
+import { randomConsistencyIndex } from './random-consistency-index';
+
+export { calculateWeights, randomConsistencyIndex, calculateConsistency };
